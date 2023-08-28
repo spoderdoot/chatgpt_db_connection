a55a2ca51789a14c12fdf7924b9ae9d0614f914d
spoderdoot <fernandopfennig@gmail.com>
2023-08-28 19:52:06 +0200

    Feature: Added ai-plugin file and logo image

--- api.py
+++ api.py
@@ -13,25 +13,25 @@ project_namespace = Namespace('Projects', description='This namespace contains t
 api.add_namespace(employee_namespace)
 api.add_namespace(project_namespace)
 
-@employee_namespace.route('/employees')
+@employee_namespace.route('')
 class Employees(Resource):
     def get(self):
         employees = get_employees_from_db()
         return {'employees': employees}
 
-@project_namespace.route('/projects')
+@project_namespace.route('')
 class Projects(Resource):
     def get(self):
         projects = get_projects_from_db()
         return {'projects': projects}
 
-@employee_namespace.route('/employee-skills')
+@employee_namespace.route('/skills')
 class EmployeeSkills(Resource):
     def get(self):
         employee_skills = get_employee_skills_from_db()
         return {'employee_skills': employee_skills}
 
-@employee_namespace.route('/employees-in-projects')
+@employee_namespace.route('/projects')
 class EmployeeProjects(Resource):
     def get(self):
         employee_in_projects = get_employees_in_projects_from_db()
