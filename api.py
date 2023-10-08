c6161c6b0366b09bbdc9fc670271dfac047fed82
spoderdoot <fernandopfennig@tutamail.com>
2023-10-08 18:02:06 +0200

    Added endpoint to send queries to, openapi file and endpoints to get API specification and logo

--- api.py
+++ api.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request, send_file
 from flask_restx import fields, Api, Resource, Namespace
 from business_logic import *
 
@@ -7,43 +7,66 @@ api = Api(app)
 
 PORT = 5000
 
-database_namespace = Namespace('Database', description='This namespace contains the database relevant endpoints.')
-employee_namespace = Namespace('Employees', description='This namespace contains the employee relevant endpoints.')
-project_namespace = Namespace('Projects', description='This namespace contains the profect relevant endpoints.')
+database_namespace = Namespace('database', description='This namespace contains the database relevant endpoints.')
+employee_namespace = Namespace('employees', description='This namespace contains the employee relevant endpoints.')
+project_namespace = Namespace('projects', description='This namespace contains the profect relevant endpoints.')
+info_namespace = Namespace('info', description='This namespace provides endpoints for ChatGPT')
 
 api.add_namespace(database_namespace)
-api.add_namespace(employee_namespace)
-api.add_namespace(project_namespace)
+#api.add_namespace(employee_namespace)
+#api.add_namespace(project_namespace)
+api.add_namespace(info_namespace)
 
-@database_namespace.route('metadata')
+model = api.model('SQLQueryModel', {
+    'query': fields.String(description='SQL Query', required=True)
+})
+
+@database_namespace.route('/metadata')
 class Database(Resource):
     def get(self):
         metadata = get_metadata_from_db()
         return metadata
+    
+@database_namespace.route('/execute_query')
+class Database(Resource):
+    @api.expect(model)
+    def post(self):
+        metadata = post_sql_query_to_db(request.json.get('query'))
+        return metadata
 
-@employee_namespace.route('')
-class Employees(Resource):
+@info_namespace.route('/openapi')
+class OpenApi(Resource):
     def get(self):
-        employees = get_employees_from_db()
-        return {'employees': employees}
+        return send_file('openapi.yaml')
 
-@project_namespace.route('')
-class Projects(Resource):
+@info_namespace.route('/logo')
+class Logo(Resource):
     def get(self):
-        projects = get_projects_from_db()
-        return {'projects': projects}
+        return send_file('logo.png', mimetype='image/png')
 
-@employee_namespace.route('/skills')
-class EmployeeSkills(Resource):
-    def get(self):
-        employee_skills = get_employee_skills_from_db()
-        return {'employee_skills': employee_skills}
+# @employee_namespace.route('')
+# class Employees(Resource):
+#     def get(self):
+#         employees = get_employees_from_db()
+#         return {'employees': employees}
 
-@employee_namespace.route('/projects')
-class EmployeeProjects(Resource):
-    def get(self):
-        employee_in_projects = get_employees_in_projects_from_db()
-        return {'employees-in-projects': employee_in_projects}
+# @project_namespace.route('')
+# class Projects(Resource):
+#     def get(self):
+#         projects = get_projects_from_db()
+#         return {'projects': projects}
+
+# @employee_namespace.route('/skills')
+# class EmployeeSkills(Resource):
+#     def get(self):
+#         employee_skills = get_employee_skills_from_db()
+#         return {'employee_skills': employee_skills}
+
+# @employee_namespace.route('/projects')
+# class EmployeeProjects(Resource):
+#     def get(self):
+#         employee_in_projects = get_employees_in_projects_from_db()
+#         return {'employees-in-projects': employee_in_projects}
 
 if __name__ == '__main__':
     app.run(port=PORT, debug=True)
--- business_logic.py
+++ business_logic.py
@@ -15,69 +15,75 @@ def get_metadata_from_db():
 
     return {'metadata': metadata}
 
-
-
-def get_employees_from_db():
-    conn = sqlite3.connect('db/gpt_organization.db')
-    cursor = conn.cursor()
-    
-    cursor.execute("SELECT name FROM t_employees")
-    employees = cursor.fetchall()
-    
-    conn.close()
-    
-    formatted_employees = [{'name': employee[0]} for employee in employees]
-    
-    return formatted_employees
-
-def get_projects_from_db():
+def post_sql_query_to_db(sqlQuery: str):
     conn = sqlite3.connect('db/gpt_organization.db')
     cursor = conn.cursor()
 
-    cursor.execute("SELECT name, budget, expenses FROM t_projects")
-    projects = cursor.fetchall()
+    cursor.execute(sqlQuery)
+    data = cursor.fetchall()
+    return {'result': data}, 200
 
-    conn.close()
+# def get_employees_from_db():
+#     conn = sqlite3.connect('db/gpt_organization.db')
+#     cursor = conn.cursor()
+    
+#     cursor.execute("SELECT name FROM t_employees")
+#     employees = cursor.fetchall()
+    
+#     conn.close()
+    
+#     formatted_employees = [{'name': employee[0]} for employee in employees]
+    
+#     return formatted_employees
 
-    formatted_projects = [{'name': project[0], 'budget': project[1], 'expenses': project[2]} for project in projects]
+# def get_projects_from_db():
+#     conn = sqlite3.connect('db/gpt_organization.db')
+#     cursor = conn.cursor()
 
-    return formatted_projects
+#     cursor.execute("SELECT name, budget, expenses FROM t_projects")
+#     projects = cursor.fetchall()
 
-def get_employee_skills_from_db():
-    conn = sqlite3.connect('db/gpt_organization.db')
-    cursor = conn.cursor()
+#     conn.close()
 
-    cursor.execute("""SELECT e.name AS employee_name, s.name AS skill_name, ss.strength AS skill_strength 
-                      FROM t_employee_skills AS es 
-                      JOIN t_employees AS e ON es.employee_id = e.id 
-                      JOIN t_skills AS s ON es.skill_id = s.id 
-                      JOIN t_skill_strengths AS ss ON es.skill_strength_id = ss.id
-                      ORDER BY es.employee_id;""")
-    employee_skills = cursor.fetchall()
+#     formatted_projects = [{'name': project[0], 'budget': project[1], 'expenses': project[2]} for project in projects]
 
-    conn.close()
+#     return formatted_projects
 
-    formatted_employee_skills = [{'name':skill[0],'skill_name':skill[1],'skill_strength':skill[2],} for skill in employee_skills]
+# def get_employee_skills_from_db():
+#     conn = sqlite3.connect('db/gpt_organization.db')
+#     cursor = conn.cursor()
 
-    return formatted_employee_skills
+#     cursor.execute("""SELECT e.name AS employee_name, s.name AS skill_name, ss.strength AS skill_strength 
+#                       FROM t_employee_skills AS es 
+#                       JOIN t_employees AS e ON es.employee_id = e.id 
+#                       JOIN t_skills AS s ON es.skill_id = s.id 
+#                       JOIN t_skill_strengths AS ss ON es.skill_strength_id = ss.id
+#                       ORDER BY es.employee_id;""")
+#     employee_skills = cursor.fetchall()
 
-def get_employees_in_projects_from_db():
-    conn = sqlite3.connect('db/gpt_organization.db')
-    cursor = conn.cursor()
+#     conn.close()
 
-    cursor.execute("""SELECT p.name as project_name, e.name as employee_name, r.name as role_name, s.name as skill_name, ss.strength as skill_strength
-                      FROM t_employees_in_projects AS ep
-                      JOIN t_employees AS e ON ep.employee_id = e.id 
-                      JOIN t_projects AS p ON ep.project_id = p.id 
-                      JOIN t_roles AS r ON ep.role_id = r.id
-                      JOIN t_project_skills_required as psr ON psr.project_id = ep.project_id
-                      JOIN t_skills as s on psr.skill_id = s.id
-                      JOIN t_skill_strengths as ss on psr.skill_strength_id = ss.id
-                      ORDER BY ep.project_id;""")
-    employees_in_projects = cursor.fetchall()
+#     formatted_employee_skills = [{'name':skill[0],'skill_name':skill[1],'skill_strength':skill[2],} for skill in employee_skills]
 
-    conn.close()
+#     return formatted_employee_skills
 
-    formatted_employee_skills = [{'project_name':employee_in_project[0],'employee_name':employee_in_project[1],'role_name':employee_in_project[2],} for employee_in_project in employees_in_projects]
+# def get_employees_in_projects_from_db():
+#     conn = sqlite3.connect('db/gpt_organization.db')
+#     cursor = conn.cursor()
 
-    return employees_in_projects
+#     cursor.execute("""SELECT p.name as project_name, e.name as employee_name, r.name as role_name, s.name as skill_name, ss.strength as skill_strength
+#                       FROM t_employees_in_projects AS ep
+#                       JOIN t_employees AS e ON ep.employee_id = e.id 
+#                       JOIN t_projects AS p ON ep.project_id = p.id 
+#                       JOIN t_roles AS r ON ep.role_id = r.id
+#                       JOIN t_project_skills_required as psr ON psr.project_id = ep.project_id
+#                       JOIN t_skills as s on psr.skill_id = s.id
+#                       JOIN t_skill_strengths as ss on psr.skill_strength_id = ss.id
+#                       ORDER BY ep.project_id;""")
+#     employees_in_projects = cursor.fetchall()
+
+#     conn.close()
+
+#     formatted_employee_skills = [{'project_name':employee_in_project[0],'employee_name':employee_in_project[1],'role_name':employee_in_project[2],} for employee_in_project in employees_in_projects]
+
+#     return employees_in_projects
